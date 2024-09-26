0ef15e9b21c746d11165ed86b0d09c45a1ba74bd
JB <julian.blacher@gmail.com>
2024-09-25 23:13:37 -0300

    fix: gap between player unrevealed cards

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -23,12 +23,22 @@ const Hand = ({ gameState, playerID, handCards, handleAction, toggleCardSelectio
         }
     }
 
-    const marginLeft = handCards.length <= 5 ? 0 : -(handCards.length - 5);
+    //const marginLeft1 = handCards.length <= 5 ? 0 : -(handCards.length - 5);
+    const marginLeft = handCards.length <= 5 ? '1vh' : `-${(handCards.length - 5) / handCards.length}vh`; // Adjust negative margin for overlapping
+
 
     return (
-    <Grid container sx={{ justifyContent: 'center', overflow: 'hidden' }}>
+    <Grid container sx={{ justifyContent: 'center', overflow: 'hidden', gap: '1vh' }}>
       {handCards.map((handCard, i) => (
-        <Grid item key={handCard.index} sx={{ flexShrink: 0, marginLeft: `${i === 0 ? 0 : marginLeft}vh` }}>
+        <Grid 
+          item
+          key={handCard.index} 
+          sx={{
+            flexShrink: 0,
+            marginLeft: `${i === 0 ? 0 : marginLeft}`,
+            zIndex: handCards.length - i // Ensure proper z-index layering for cards
+          }}
+        >
           <Card 
             id={handCard.card.id}
             displayName={handCard.card.displayName}
