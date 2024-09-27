a21a39c96f7add7c8a2d99ed3da6a678d011f8c3
JB <julian.blacher@gmail.com>
2024-09-26 23:31:52 -0300

    fix: now modal is triggered only to the current player

--- src/components/Game.js
+++ src/components/Game.js
@@ -69,6 +69,8 @@ export const Game = ({ manager }) => {
     return null;
   }
 
+  const isCurrentPlayerTurn = gameState.youPlayerID === gameState.turnPlayerID;
+
   return (
     <Box sx={{ width: '100%', height: '100vh', flexGrow: 1 }}>
       <Grid container spacing={0} columns={3} sx={{ height: '100%', width: '100%' }}>
@@ -92,74 +94,79 @@ export const Game = ({ manager }) => {
 
         </Grid>
         {/* Moat modal */}
-        <ConquestDialog
-          open={hasMoatActions(gameState)}
-          dialogTitle="Use Moat?"
-          happyButton={{ label: "Yes", handleAction: () => handleAction(yesAction(gameState)) }}
-          sadButton={{ label: "No", handleAction: () => handleAction(noAction(gameState)) }}
-          handCards={gameState.players[gameState.youPlayerID].hand.handCards.filter(handCard => handCard.card.id === "moat")}
-          setTrigger={setTrigger}
-        />
-        {/* Library modal */}
-        <ConquestDialog
-          open={hasActionsOfKinds(gameState, ["keep_card"])}
-          dialogTitle="Keep this action card?"
-          happyButton={{ label: "Yes", handleAction: () => handleAction(keepAction(gameState, true)) }}
-          sadButton={{ label: "No", handleAction: () => handleAction(keepAction(gameState, false)) }}
-          handCards={keepAction(gameState, true).hand_card ? [keepAction(gameState, true).hand_card] : []}
-          setTrigger={setTrigger}
-        />
-        {/* Bandit modal */}
-        <ConquestDialog
-          open={hasActionsOfKinds(gameState, ["discard_and_trash_cards"])}
-          dialogTitle="Choose which treasure card to trash"
-          happyButton={{ label: "Trash selected", handleAction: () => {
-            if (manager.conquestDialogSelectedCards.length !== 1) {
-              return;
+        {isCurrentPlayerTurn && (
+          <>
+            <ConquestDialog
+              open={hasMoatActions(gameState)}
+            dialogTitle="Use Moat?"
+            happyButton={{ label: "Yes", handleAction: () => handleAction(yesAction(gameState)) }}
+            sadButton={{ label: "No", handleAction: () => handleAction(noAction(gameState)) }}
+            handCards={gameState.players[gameState.youPlayerID].hand.handCards.filter(handCard => handCard.card.id === "moat")}
+            setTrigger={setTrigger}
+          />
+          {/* Library modal */}
+          <ConquestDialog
+            open={hasActionsOfKinds(gameState, ["keep_card"])}
+            dialogTitle="Keep this action card?"
+            happyButton={{ label: "Yes", handleAction: () => handleAction(keepAction(gameState, true)) }}
+            sadButton={{ label: "No", handleAction: () => handleAction(keepAction(gameState, false)) }}
+            handCards={keepAction(gameState, true).hand_card ? [keepAction(gameState, true).hand_card] : []}
+            setTrigger={setTrigger}
+          />
+          {/* Bandit modal */}
+          <ConquestDialog
+            open={hasActionsOfKinds(gameState, ["discard_and_trash_cards"])}
+            dialogTitle="Choose which treasure card to trash"
+            happyButton={{ label: "Trash selected", handleAction: () => {
+              if (manager.conquestDialogSelectedCards.length !== 1) {
+                return;
+              }
+              const selectedHandCard = manager.conquestDialogSelectedCards[0];
+              const action = getActionOfKinds(gameState, ["discard_and_trash_cards"])  
+              const handCards = [action.discard_hand_card, action.trash_hand_card];
+              const trashHandCard = handCards.find(hc => hc.card.index === selectedHandCard.index);
+              const discardHandCard = handCards.find(hc => hc.card.index !== selectedHandCard.index);
+                handleAction(discardAndTrashCardsAction(gameState, discardHandCard, trashHandCard)) 
+              }
+            }}
+            handCards={
+              (() => {
+                const action = getActionOfKinds(gameState, ["discard_and_trash_cards"]);
+                return action ? [action.discard_hand_card, action.trash_hand_card] : [];
+              })()
             }
-            const selectedHandCard = manager.conquestDialogSelectedCards[0];
-            const action = getActionOfKinds(gameState, ["discard_and_trash_cards"])  
-            const handCards = [action.discard_hand_card, action.trash_hand_card];
-            const trashHandCard = handCards.find(hc => hc.card.index === selectedHandCard.index);
-            const discardHandCard = handCards.find(hc => hc.card.index !== selectedHandCard.index);
-              handleAction(discardAndTrashCardsAction(gameState, discardHandCard, trashHandCard)) 
-            }
-          }}
-          handCards={
-            (() => {
-              const action = getActionOfKinds(gameState, ["discard_and_trash_cards"]);
-              return action ? [action.discard_hand_card, action.trash_hand_card] : [];
-            })()
-          }
-          setTrigger={setTrigger}
-          canSelectCards={true}
-          exactly_n={1}
-          conquestDialogSelectedCards={manager.conquestDialogSelectedCards}
-          setConquestDialogSelectedCards={(cs) => manager.setConquestDialogSelectedCards(cs)}
-        />
-        {/* Vassal modal */}
-        <ConquestDialog
-          open={hasVassalActions(gameState)}
-          dialogTitle="Play this action card?"
-          happyButton={{ label: "Yes", handleAction: () => handleAction(getActionOfKinds(gameState, ['yes'])) }}
-          sadButton={{ label: "No", handleAction: () => handleAction(getActionOfKinds(gameState, ['no'])) }}
-          handCards={[gameState.players[gameState.youPlayerID].hand.handCards[gameState.players[gameState.youPlayerID].hand.handCards.length - 1]]}
-          setTrigger={setTrigger}
-        />
-        {/* Harbinger modal */}
-        <ConquestDialog
-          open={hasActionsOfKinds(gameState, ['move_discarded_to_deck'])}
-          dialogTitle="Which card do you want to topdeck?"
-          happyButton={{ label: "Topdeck", handleAction: () => handleAction(moveDiscardedToDeckAction(gameState, manager.conquestDialogSelectedCards[0])) }}
-          // TODO: Harbinger has no way to not topdeck
-          // sadButton={{ label: "Don't Topdeck", action: () => handleAction(getActionOfKinds(gameState, ['move_discarded_to_deck'])) }}
-          cards={gameState.players[gameState.youPlayerID].discardPile.cards}
-          setTrigger={setTrigger}
-          canSelectCards={true}
-          exactly_n={1}
-          conquestDialogSelectedCards={manager.conquestDialogSelectedCards}
-          setConquestDialogSelectedCards={(cs) => manager.setConquestDialogSelectedCards(cs)}
-        />
+            setTrigger={setTrigger}
+            canSelectCards={true}
+            exactly_n={1}
+            conquestDialogSelectedCards={manager.conquestDialogSelectedCards}
+            setConquestDialogSelectedCards={(cs) => manager.setConquestDialogSelectedCards(cs)}
+          />
+          {/* Vassal modal */}
+          <ConquestDialog
+            open={hasVassalActions(gameState)}
+            dialogTitle="Play this action card?"
+            happyButton={{ label: "Yes", handleAction: () => handleAction(getActionOfKinds(gameState, ['yes'])) }}
+            sadButton={{ label: "No", handleAction: () => handleAction(getActionOfKinds(gameState, ['no'])) }}
+            handCards={[gameState.players[gameState.youPlayerID].hand.handCards[gameState.players[gameState.youPlayerID].hand.handCards.length - 1]]}
+            setTrigger={setTrigger}
+          />
+          {/* Harbinger modal */}
+          <ConquestDialog
+            open={hasActionsOfKinds(gameState, ['move_discarded_to_deck'])}
+            dialogTitle="Which card do you want to topdeck?"
+            happyButton={{ label: "Topdeck", handleAction: () => handleAction(moveDiscardedToDeckAction(gameState, manager.conquestDialogSelectedCards[0])) }}
+            // TODO: Harbinger has no way to not topdeck
+            // sadButton={{ label: "Don't Topdeck", action: () => handleAction(getActionOfKinds(gameState, ['move_discarded_to_deck'])) }}
+            cards={gameState.players[gameState.youPlayerID].discardPile.cards}
+            setTrigger={setTrigger}
+            canSelectCards={true}
+            exactly_n={1}
+            conquestDialogSelectedCards={manager.conquestDialogSelectedCards}
+            setConquestDialogSelectedCards={(cs) => manager.setConquestDialogSelectedCards(cs)}
+          />
+        </>
+        )}
+
       </Grid>
     </Box>
   );
